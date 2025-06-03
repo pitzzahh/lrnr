98060449521c8e032252bde81913c790157d70a8
Peter John Arao <pitzzahh@gmail.com>
2025-06-03 16:00:39 +0800

    refactor: remove OpenAPIHono initialization and related code

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,5 @@
-import { OpenAPIHono } from '@hono/zod-openapi';
 import { serve } from 'bun';
-
-const app = new OpenAPIHono();
-
-app.get("/", (c) => {
-  return c.text("Hello, world!");
-})
+import app from '@/app';
 
 const port = process.env.PORT || 3000;
 console.log(`Server is running on http://localhost:${port}`);
