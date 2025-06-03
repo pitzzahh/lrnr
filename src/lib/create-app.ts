a15f268b07ff67fe7161e52d325ed382cce76c0a
Peter John Arao <pitzzahh@gmail.com>
2025-06-04 00:05:24 +0800

    feat: add defaultHook to OpenAPIHono router configuration

--- src/lib/create-app.ts
+++ src/lib/create-app.ts
@@ -2,9 +2,10 @@ import { OpenAPIHono } from '@hono/zod-openapi';
 import { notFound, onError, serveEmojiFavicon } from 'stoker/middlewares';
 import { logger } from '@/hooks/pino-logger';
 import { AppBindings } from '@/lib/types/app';
+import { defaultHook } from 'stoker/openapi';
 
 export function createRouter() {
-  return new OpenAPIHono<AppBindings>({ strict: false });
+  return new OpenAPIHono<AppBindings>({ strict: false, defaultHook: defaultHook });
 }
 
 export default function createApp() {
