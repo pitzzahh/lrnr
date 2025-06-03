289bd446608bedcd84e03985a157a797930b3065
Peter John Arao <pitzzahh@gmail.com>
2025-06-03 16:24:51 +0800

    feat: add pino logger implementation with Hono integration

---
+++ src/pino-logger.ts
@@ -0,0 +1,16 @@
+import { pinoLogger } from "hono-pino";
+import pino from 'pino';
+import pretty = require('pino-pretty');
+
+export function logger() {
+  return pinoLogger({
+    pino: pino(pretty({
+      colorize: true,
+      translateTime: 'SYS:standard',
+      ignore: 'pid,hostname'
+    })),
+    http: {
+      referRequestIdKey: 'referer',
+    }
+  });
+}
