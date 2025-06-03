50506e9d5fef414a374a3a07f124659b557825e4
Peter John Arao <pitzzahh@gmail.com>
2025-06-03 23:27:50 +0800

    refactor: organize route imports and setup in app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,13 @@
 import createApp from '@/lib/create-app';
 import configureOpenAPI from '@/lib/configure-openapi';
-
+import index from '@/routes/index.route';
 const app = createApp();
 
+const routes = [
+  index
+]
+
 configureOpenAPI(app);
+routes.forEach(route => (app.route("/", route)));
 
 export default app;
