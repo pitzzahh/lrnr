bf797236e674038c9c2c9d36dd03f989ac43df9e
Peter John Arao <pitzzahh@gmail.com>
2025-06-03 22:24:13 +0800

    feat: enhance OpenAPIHono initialization with AppBindings type definition

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import { OpenAPIHono } from '@hono/zod-openapi';
 import { notFound, onError } from 'stoker/middlewares';
 import { logger } from '@/pino-logger';
+import { AppBindings } from '@/types/app-bindings';
 
-const app = new OpenAPIHono();
+const app = new OpenAPIHono<AppBindings>();
 app.use(logger());
 
 app.get("/", (c) => {
+  c.var.logger.info("Root endpoint accessed");
   return c.text("Hello, world!");
 })
 
