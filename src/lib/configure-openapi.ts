829971900c71ae865286b1453ee4373172689e4b
Peter John Arao <pitzzahh@gmail.com>
2025-06-04 00:04:33 +0800

    feat: enhance OpenAPI configuration with markdown generation and llms route

--- src/lib/configure-openapi.ts
+++ src/lib/configure-openapi.ts
@@ -1,8 +1,21 @@
 import type { AppOpenAPI } from "@/lib/types/app";
 import pj from "../../package.json"
 import { Scalar } from "@scalar/hono-api-reference";
+import { createMarkdownFromOpenApi } from "@scalar/openapi-to-markdown";
 
-export default function configureOpenAPI(app: AppOpenAPI) {
+export default async function configureOpenAPI(app: AppOpenAPI) {
+
+  const content = app.getOpenAPI31Document({
+    openapi: '3.1.0',
+    info: { title: 'Example', version: 'v1' },
+  })
+
+  const markdown = await createMarkdownFromOpenApi(JSON.stringify(content))
+
+  app.get('/llms.txt', async (c) => {
+    return c.text(markdown)
+  })
+
   app.doc("/doc", {
     openapi: "3.0.0",
     info: {
@@ -11,5 +24,16 @@ export default function configureOpenAPI(app: AppOpenAPI) {
       version: pj.version
     },
   });
-  app.get("/reference", Scalar({ url: '/doc' }))
+  app.get("/reference",
+    Scalar({
+      spec: {
+        url: '/doc',
+      },
+      theme: "kepler",
+      defaultHttpClient: {
+        targetKey: "javascript",
+        clientKey: "fetch",
+      }
+    })
+  );
 }
