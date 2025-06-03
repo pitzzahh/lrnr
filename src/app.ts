f3c5e7c919be984c0245b3693e6a47cbf2653c41
Peter John Arao <pitzzahh@gmail.com>
2025-06-03 16:24:57 +0800

    feat: integrate stoker middlewares and pino logger into the app

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,20 @@
 import { OpenAPIHono } from '@hono/zod-openapi';
+import { notFound, onError } from 'stoker/middlewares';
+import { logger } from '@/pino-logger';
 
 const app = new OpenAPIHono();
 
 app.get("/", (c) => {
   return c.text("Hello, world!");
 })
+
+app.get("/health", (c) => {
+  throw new Error("This is a test error");
+});
+
+app.notFound(notFound);
+app.onError(onError);
+
+app.use(logger())
 
 export default app;
