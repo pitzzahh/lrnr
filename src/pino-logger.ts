f04717c31ea9c0c25d6cca6155347b4fe412b61b
Peter John Arao <pitzzahh@gmail.com>
2025-06-03 22:22:35 +0800

    fix: update logger to use environment configuration for log level

--- src/pino-logger.ts
+++ src/pino-logger.ts
@@ -1,11 +1,12 @@
 import { pinoLogger } from "hono-pino";
 import pino from 'pino';
 import pretty = require('pino-pretty');
+import env from "@/env";
 
 export function logger() {
   return pinoLogger({
     pino: pino({
-      level: process.env.LOG_LEVEL || 'info',
+      level: env.LOG_LEVEL || 'info',
     }, pretty({
       colorize: true,
       translateTime: 'SYS:standard',
