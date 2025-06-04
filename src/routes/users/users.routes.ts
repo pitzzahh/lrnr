4901adf01343031277642fd718f519a22d7f5a69
Peter John Arao <pitzzahh@gmail.com>
2025-06-04 15:29:08 +0800

    fix: remove unnecessary blank line in users.routes.ts

--- src/routes/users/users.routes.ts
+++ src/routes/users/users.routes.ts
@@ -2,7 +2,6 @@ import { SELECT_USERS_SCHEMA } from '@/db/schema/users'
 import { createRoute, z } from '@hono/zod-openapi'
 import * as HttpStatusCodes from 'stoker/http-status-codes'
 import { jsonContent } from 'stoker/openapi/helpers'
-
 const tags = ['Users']
 
 export const list = createRoute({
