c93cf060955f119970c3ecee9f4f2e250f2ae3f8
Peter John Arao <pitzzahh@gmail.com>
2025-06-04 15:06:40 +0800

    feat: add categories table and update users table schema

---
+++ src/db/migrations/0002_polite_ravenous.sql
@@ -0,0 +1,7 @@
+CREATE TABLE "categories" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"name" text NOT NULL,
+	CONSTRAINT "categories_name_unique" UNIQUE("name")
+);
+--> statement-breakpoint
+ALTER TABLE "users" RENAME COLUMN "passwordHash" TO "password_hash";
