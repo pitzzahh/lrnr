3b21152aefe87c589b9eebdaec5c6a24fc2cd4de
Peter John Arao <pitzzahh@gmail.com>
2025-06-04 13:23:29 +0800

    refactor: reorder import statement for env in pino-logger file

--- src/hooks/pino-logger.ts
+++ src/hooks/pino-logger.ts
@@ -1,7 +1,7 @@
+import env from '@/env'
 import { pinoLogger } from 'hono-pino'
 import pino from 'pino'
 import pretty from 'pino-pretty'
-import env from '@/env'
 
 export function logger() {
 	return pinoLogger({
