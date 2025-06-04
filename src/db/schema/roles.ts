1961fe5039597a1ce649ed346fdbaba8dffda5d4
Peter John Arao <pitzzahh@gmail.com>
2025-06-04 13:23:12 +0800

    refactor: reorder import statements for consistency in roles schema file

--- src/db/schema/roles.ts
+++ src/db/schema/roles.ts
@@ -1,5 +1,5 @@
-import { pgTable } from 'drizzle-orm/pg-core'
 import roleName from '@/db/schema/enums/role-name'
+import { pgTable } from 'drizzle-orm/pg-core'
 
 const roles = pgTable('roles', (t) => ({
 	id: t.text('id').primaryKey(),
