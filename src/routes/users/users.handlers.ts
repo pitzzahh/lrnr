2a59b90a8d5d003a9bad7deef256931e4730e4be
Peter John Arao <pitzzahh@gmail.com>
2025-06-04 16:05:23 +0800

    feat: add create user handler to users routes

--- src/routes/users/users.handlers.ts
+++ src/routes/users/users.handlers.ts
@@ -1,7 +1,15 @@
 import db from '@/db'
 import type { AppRouteHandler } from '@/lib/types'
-import type { ListRoute } from './users.routes'
+import type { ListRoute, CreateRoute } from './users.routes'
+import { users } from '@/db/schema'
+import * as HttpStatusCodes from 'stoker/http-status-codes'
 
 export const list: AppRouteHandler<ListRoute> = async (c) => {
 	return c.json(await db.query.users.findMany())
+}
+
+export const create: AppRouteHandler<CreateRoute> = async (c) => {
+	const user = c.req.valid('json')
+	const [inserted_user] = await db.insert(users).values(user).returning()
+	return c.json(inserted_user, HttpStatusCodes.OK)
 }
