a8cf2a3ae4aedb9ad960f3b1e7d5184037cc62e7
Peter John Arao <pitzzahh@gmail.com>
2025-06-04 16:05:47 +0800

    feat: add create route handler to users router

--- src/routes/users/users.index.ts
+++ src/routes/users/users.index.ts
@@ -2,6 +2,8 @@ import { createRouter } from '@/lib/create-app'
 import * as handlers from './users.handlers'
 import * as routes from './users.routes'
 
-const router = createRouter().openapi(routes.list, handlers.list)
+const router = createRouter()
+	.openapi(routes.list, handlers.list)
+	.openapi(routes.create, handlers.create)
 
 export default router
