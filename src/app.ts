8be56c250faa985a4ecd91a5d84f0edd1e056391
Peter John Arao <pitzzahh@gmail.com>
2025-06-04 14:49:08 +0800

    feat: add users route to the application for user-related API handling

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,15 @@ import configureOpenAPI from '@/lib/configure-openapi'
 import createApp from '@/lib/create-app'
 import index from '@/routes/index.route'
 import llms from '@/routes/llms/llms.route'
+import users from "@/routes/users/users.index"
 const app = createApp()
 
-const routes = [index, llms]
+const routes = [index, llms, users]
 await configureOpenAPI(app)
 for (const route of routes) {
 	app.route('/', route)
 }
 
-export default app
+export type AppType = typeof routes[number];
+
+export default app;
