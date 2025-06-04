0cdeb6eb30c4735479548bf704cc1073edeacdf2
Peter John Arao <pitzzahh@gmail.com>
2025-06-04 16:18:14 +0800

    feat: add getOne route handler to users routes

--- src/routes/users/users.handlers.ts
+++ src/routes/users/users.handlers.ts
@@ -1,8 +1,9 @@
 import db from '@/db'
 import type { AppRouteHandler } from '@/lib/types'
-import type { ListRoute, CreateRoute } from './users.routes'
+import type { ListRoute, CreateRoute, GetOneRoute } from './users.routes'
 import { users } from '@/db/schema'
 import * as HttpStatusCodes from 'stoker/http-status-codes'
+import * as HttpStatusPhrases from 'stoker/http-status-phrases'
 
 export const list: AppRouteHandler<ListRoute> = async (c) => {
 	return c.json(await db.query.users.findMany())
@@ -12,4 +13,22 @@ export const create: AppRouteHandler<CreateRoute> = async (c) => {
 	const user = c.req.valid('json')
 	const [inserted_user] = await db.insert(users).values(user).returning()
 	return c.json(inserted_user, HttpStatusCodes.OK)
+}
+
+export const getOne: AppRouteHandler<GetOneRoute> = async (c) => {
+	const { id } = c.req.valid('param')
+	const user = await db.query.users.findFirst({
+		where(fields, operators) {
+			return operators.eq(fields.id, id)
+		},
+	})
+	if (!user) {
+		return c.json(
+			{
+				message: HttpStatusPhrases.NOT_FOUND,
+			},
+			HttpStatusCodes.NOT_FOUND
+		)
+	}
+	return c.json(user, HttpStatusCodes.OK)
 }
--- src/routes/users/users.routes.ts
+++ src/routes/users/users.routes.ts
@@ -1,8 +1,9 @@
 import { INSERT_USERS_SCHEMA, SELECT_USERS_SCHEMA } from '@/db/schema/users'
+import { NOT_FOUND_SCHEMA } from '@/lib/constants'
 import { createRoute, z } from '@hono/zod-openapi'
 import * as HttpStatusCodes from 'stoker/http-status-codes'
 import { jsonContent, jsonContentRequired } from 'stoker/openapi/helpers'
-import { createErrorSchema } from 'stoker/openapi/schemas'
+import { createErrorSchema, IdUUIDParamsSchema } from 'stoker/openapi/schemas'
 const tags = ['Users']
 const path = '/users'
 
@@ -31,5 +32,23 @@ export const create = createRoute({
 	},
 })
 
+export const getOne = createRoute({
+	path: `${path}/{id}`,
+	method: 'get',
+	request: {
+		params: IdUUIDParamsSchema,
+	},
+	tags,
+	responses: {
+		[HttpStatusCodes.OK]: jsonContent(SELECT_USERS_SCHEMA, 'The requested user'),
+		[HttpStatusCodes.NOT_FOUND]: jsonContent(NOT_FOUND_SCHEMA, 'Request user not found'),
+		[HttpStatusCodes.UNPROCESSABLE_ENTITY]: jsonContent(
+			createErrorSchema(IdUUIDParamsSchema),
+			'Invalid id error'
+		),
+	},
+})
+
 export type ListRoute = typeof list
 export type CreateRoute = typeof create
+export type GetOneRoute = typeof getOne
