89ccd8905dd41be86998684de3b404f7f94d7966
Peter John Arao <pitzzahh@gmail.com>
2025-06-04 15:37:02 +0800

    feat: add categories and courses routes to the application

--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,11 @@ import createApp from '@/lib/create-app'
 import index from '@/routes/index.route'
 import llms from '@/routes/llms/llms.route'
 import users from '@/routes/users/users.index'
+import categories from '@/routes/categories/categories.index'
+import courses from '@/routes/courses/courses.index'
 const app = createApp()
 
-const routes = [index, llms, users]
+const routes = [index, llms, users, categories, courses]
 await configureOpenAPI(app)
 for (const route of routes) {
 	app.route('/', route)
