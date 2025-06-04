0b33ddbcf23c4fcd8f0956621f9d6b2711445d62
Peter John Arao <pitzzahh@gmail.com>
2025-06-04 11:57:53 +0800

    feat: remove TypeScript ignore comment from app.ts

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,6 @@ import llms from '@/routes/llms/llms.route'
 const app = createApp()
 
 const routes = [index, llms]
-// @ts-ignore
 await configureOpenAPI(app)
 for (const route of routes) {
 	app.route('/', route)
