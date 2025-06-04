16445c75f24f54f57563c8a61e64aeb71f35d575
Peter John Arao <pitzzahh@gmail.com>
2025-06-04 14:59:01 +0800

    fix: correct schema reference in user list route response

--- src/routes/users/users.routes.ts
+++ src/routes/users/users.routes.ts
@@ -1,9 +1,7 @@
+import { SELECT_USERS_SCHEMA } from '@/db/schema/users'
 import { createRoute, z } from '@hono/zod-openapi'
 import * as HttpStatusCodes from 'stoker/http-status-codes'
-import { jsonContent, jsonContentRequired } from 'stoker/openapi/helpers'
-import { createErrorSchema, IdParamsSchema } from 'stoker/openapi/schemas'
-import { InsertUsersSchema, PatchUsersSchema, SelectUsersSchema } from '@/db/schema/users'
-import { NOT_FOUND_SCHEMA } from '@/lib/constants'
+import { jsonContent } from 'stoker/openapi/helpers'
 
 const tags = ['Users']
 
@@ -12,8 +10,8 @@ export const list = createRoute({
 	method: 'get',
 	tags,
 	responses: {
-		[HttpStatusCodes.OK]: jsonContent(z.array(SelectUsersSchema), 'The list of users'),
+		[HttpStatusCodes.OK]: jsonContent(z.array(SELECT_USERS_SCHEMA), 'The list of users'),
 	},
 })
 
-export type ListRoute = typeof list;
+export type ListRoute = typeof list
