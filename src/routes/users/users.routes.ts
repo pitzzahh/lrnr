0211966fe0ae7408435d79c2d3afcb2594bfac7d
Peter John Arao <pitzzahh@gmail.com>
2025-06-04 16:05:37 +0800

    feat: add create user route and handler to users routes

--- src/routes/users/users.routes.ts
+++ src/routes/users/users.routes.ts
@@ -1,11 +1,13 @@
-import { SELECT_USERS_SCHEMA } from '@/db/schema/users'
+import { INSERT_USERS_SCHEMA, SELECT_USERS_SCHEMA } from '@/db/schema/users'
 import { createRoute, z } from '@hono/zod-openapi'
 import * as HttpStatusCodes from 'stoker/http-status-codes'
-import { jsonContent } from 'stoker/openapi/helpers'
+import { jsonContent, jsonContentRequired } from 'stoker/openapi/helpers'
+import { createErrorSchema } from 'stoker/openapi/schemas'
 const tags = ['Users']
+const path = '/users'
 
 export const list = createRoute({
-	path: '/users',
+	path,
 	method: 'get',
 	tags,
 	responses: {
@@ -13,4 +15,21 @@ export const list = createRoute({
 	},
 })
 
+export const create = createRoute({
+	path,
+	method: 'post',
+	tags,
+	request: {
+		body: jsonContentRequired(INSERT_USERS_SCHEMA, 'The user to create'),
+	},
+	responses: {
+		[HttpStatusCodes.OK]: jsonContent(SELECT_USERS_SCHEMA, 'The created user'),
+		[HttpStatusCodes.UNPROCESSABLE_ENTITY]: jsonContent(
+			createErrorSchema(INSERT_USERS_SCHEMA),
+			'The validation error(s) for the user creation request'
+		),
+	},
+})
+
 export type ListRoute = typeof list
+export type CreateRoute = typeof create
