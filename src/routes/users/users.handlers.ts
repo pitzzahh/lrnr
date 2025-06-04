11a1fca166bcebe78cfa5bb4a4108a62aa9791bd
Peter John Arao <pitzzahh@gmail.com>
2025-06-04 14:58:37 +0800

    refactor: remove unused imports from user list handler

--- src/routes/users/users.handlers.ts
+++ src/routes/users/users.handlers.ts
@@ -1,10 +1,5 @@
 import db from '@/db'
-import { users } from '@/db/schema'
 import type { AppRouteHandler } from '@/lib/types'
-import { eq } from 'drizzle-orm'
-import * as HttpStatusCodes from 'stoker/http-status-codes'
-import * as HttpStatusPhrases from 'stoker/http-status-phrases'
-import { ZOD_ERROR_CODES, ZOD_ERROR_MESSAGES } from '@/lib/constants'
 import type { ListRoute } from './users.routes'
 
 export const list: AppRouteHandler<ListRoute> = async (c) => {
