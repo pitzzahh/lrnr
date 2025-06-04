368610151064f000518bec7b7ffea04d6eb60ebe
Peter John Arao <pitzzahh@gmail.com>
2025-06-04 12:47:21 +0800

    feat: update schema exports to include role_name and courses

--- src/db/schema/index.ts
+++ src/db/schema/index.ts
@@ -1,1 +1,3 @@
-export { default as user } from './users';
+export { default as role_name } from './enums/role-name'
+export { default as users } from './users'
+export { default as courses } from './courses'
