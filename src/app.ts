2326390551268a48ef0dbd0792af23d46cdf50fa
Peter John Arao <pitzzahh@gmail.com>
2025-06-05 21:28:23 +0800

    feat: add enrollments route to the main application

--- src/app.ts
+++ src/app.ts
@@ -3,12 +3,13 @@ import createApp from '@/lib/create-app'
 import auth from '@/routes/auth/auth.index'
 import categories from '@/routes/categories/categories.index'
 import courses from '@/routes/courses/courses.index'
+import enrollments from '@/routes/enrollments/enrollments.index'
 import index from '@/routes/index.route'
 import llms from '@/routes/llms/llms.route'
 import users from '@/routes/users/users.index'
 const app = createApp()
 
-const routes = [index, auth, users, categories, courses, llms]
+const routes = [index, auth, users, categories, courses, enrollments, llms]
 
 for (const route of routes) {
 	app.route('/', route)
