96224f816bc59c64d70c29d6f1e484fecc8db66b
Peter John Arao <pitzzahh@gmail.com>
2025-06-05 16:50:29 +0800

    feat: restructure auth router to include route handlers for signin, signup, and logout

--- src/routes/auth/auth.index.ts
+++ src/routes/auth/auth.index.ts
@@ -1,5 +1,9 @@
 import { createRouter } from '@/lib/create-app'
-
+import * as handlers from './auth.handlers'
+import * as routes from './auth.routes'
 const router = createRouter()
+	.openapi(routes.signin, handlers.signin)
+	.openapi(routes.signup, handlers.signup)
+	.openapi(routes.logout, handlers.logout)
 
 export default router
