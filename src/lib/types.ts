c201995d0af9830b8c530a6f78809b5f4bec18fc
Peter John Arao <pitzzahh@gmail.com>
2025-06-05 14:34:12 +0800

    feat: update AppBindings to include user and session types

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,9 +1,13 @@
+import type { Sessions } from '@/db/schema/sessions'
+import type { Users } from '@/db/schema/users'
 import type { OpenAPIHono, RouteConfig, RouteHandler } from '@hono/zod-openapi'
 import type { PinoLogger } from 'hono-pino'
 
 export type AppBindings = {
 	Variables: {
 		logger: PinoLogger
+		user: Users | null
+		session: Sessions | null
 	}
 }
 export type AppOpenAPI = OpenAPIHono<AppBindings>
