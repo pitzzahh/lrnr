63b9dd39801fba62e37ccd06cee27214d9f9fcac
Peter John Arao <pitzzahh@gmail.com>
2025-06-05 14:33:59 +0800

    feat: implement authentication middleware for session management

---
+++ src/hooks/auth.ts
@@ -0,0 +1,32 @@
+import {
+	SESSION_COOKIE_NAME,
+	deleteSessionTokenCookie,
+	generate_session_token,
+	setSessionTokenCookie,
+	validate_session_token,
+} from '@/lib/auth'
+import type { AppBindings } from '@/lib/types'
+import { getCookie } from 'hono/cookie'
+import { createMiddleware } from 'hono/factory'
+
+export const authentication = createMiddleware<AppBindings>(async (c, next) => {
+	const sessionId = getCookie(c, SESSION_COOKIE_NAME) ?? null
+	console.log('middlearwe running')
+	console.log(sessionId)
+	if (!sessionId) {
+		c.set('user', null)
+		c.set('session', null)
+		return next()
+	}
+	const { session, user } = await validate_session_token(sessionId, c)
+	if (session && !c.req.path.startsWith('/auth')) {
+		const token = generate_session_token()
+		setSessionTokenCookie(c, token, session.expires_at)
+	}
+	if (!session) {
+		deleteSessionTokenCookie(c)
+	}
+	c.set('user', user)
+	c.set('session', session)
+	return next()
+})
