282ae332946bded35ef091040d621238e18ea4c8
Peter John Arao <pitzzahh@gmail.com>
2025-06-05 17:14:59 +0800

    feat: enhance courses_relations to include teacher and enrollments relations

--- src/db/schema/relations/courses-relations.ts
+++ src/db/schema/relations/courses-relations.ts
@@ -1,12 +1,18 @@
 import { relations } from 'drizzle-orm'
-import { categories, courses } from '..'
+import { categories, courses, enrollments, users } from '..'
 
-export const courses_relations = relations(courses, ({ one }) => {
+export const courses_relations = relations(courses, ({ one, many }) => {
 	return {
-		categories: one(categories, {
+		category: one(categories, {
 			fields: [courses.category_id],
 			references: [categories.id],
 		}),
+		teacher: one(users, {
+			fields: [courses.teacher_id],
+			references: [users.id],
+			relationName: 'teacher_courses',
+		}),
+		enrollments: many(enrollments),
 	}
 })
 
