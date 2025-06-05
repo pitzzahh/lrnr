de2071a3f05cedae5cc0537b5a4e60b29a6e3465
Peter John Arao <pitzzahh@gmail.com>
2025-06-05 19:39:54 +0800

    fix: update user type to UserRedactedPassword to enhance security by omitting password_hash

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,12 +1,12 @@
 import type { Session } from '@/db/schema/sessions'
-import type { User } from '@/db/schema/users'
+import type { UserRedactedPassword } from '@/db/schema/users'
 import type { OpenAPIHono, RouteConfig, RouteHandler } from '@hono/zod-openapi'
 import type { PinoLogger } from 'hono-pino'
 
 export type AppBindings = {
 	Variables: {
 		logger: PinoLogger
-		user: User | null
+		user: UserRedactedPassword | null
 		session: Session | null
 	}
 }
