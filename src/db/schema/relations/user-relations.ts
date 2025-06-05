14940b046c88c3a605e5e3065c8fa80354395f5d
Peter John Arao <pitzzahh@gmail.com>
2025-06-05 17:15:18 +0800

    feat: update user relations to include courses and enrollments

--- src/db/schema/relations/user-relations.ts
+++ src/db/schema/relations/user-relations.ts
@@ -1,9 +1,13 @@
 import { relations } from 'drizzle-orm'
-import { sessions, users } from '..'
+import { courses, enrollments, sessions, users } from '..'
 
 const user_relations = relations(users, ({ many }) => {
 	return {
 		sessions: many(sessions),
+		enrollments: many(enrollments),
+		courses: many(courses, {
+			relationName: 'teacher_courses',
+		}),
 	}
 })
 
