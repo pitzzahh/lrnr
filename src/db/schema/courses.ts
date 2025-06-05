a08b462fc607feebc0e9a24ff82401779895cff8
Peter John Arao <pitzzahh@gmail.com>
2025-06-05 15:36:55 +0800

    feat: add category reference and update course schema structure

--- src/db/schema/courses.ts
+++ src/db/schema/courses.ts
@@ -1,13 +1,22 @@
 import { pgTable } from 'drizzle-orm/pg-core'
 import { createInsertSchema, createSelectSchema } from 'drizzle-zod'
-import { users } from '.'
+import { users, categories } from '.'
 
 const courses = pgTable('courses', (t) => ({
 	id: t.uuid().primaryKey().defaultRandom(),
 	title: t.text().notNull(),
 	description: t.text(),
-	created_by: t.uuid().references(() => users.id),
 	created_at: t.timestamp().defaultNow(),
+	updated_at: t
+		.timestamp()
+		.defaultNow()
+		.$onUpdate(() => new Date()),
+	thumbnail_url: t.text().notNull(),
+	teacher_id: t.uuid().references(() => users.id),
+	category_id: t.uuid().references(() => categories.id, {
+		onDelete: 'set null',
+		onUpdate: 'cascade',
+	}),
 }))
 
 export const SELECT_COURSES_SCHEMA = createSelectSchema(courses)
